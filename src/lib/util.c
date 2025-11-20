8994f777e59e23e1df39c75aa119cd6beb49eab7
secnotes <1321993383@qq.com>
2025-11-20 07:22:34 -0500

    Fix some errors

--- src/lib/util.c
+++ src/lib/util.c
@@ -11,3 +11,16 @@
 uint64_t align_page(uint64_t address) {
     return ((address + ONE_PAGE - 1) & ~(ONE_PAGE - 1));
 }
+
+/**
+ * @brief 根据虚拟地址计算符合条件的偏移地址
+ * calculate eligible offset addresses based on virtual addresses
+ * @param p_offset input segment file offset
+ * @param p_vaddr input segment address
+ * @return uint64_t output 4k address
+ */
+uint64_t align_offset(uint64_t p_offset, uint64_t p_vaddr) {
+    if (ONE_PAGE == 0) return p_offset;
+    uint64_t remainder_diff = (p_vaddr - p_offset) % ONE_PAGE;
+    return p_offset + (remainder_diff & (ONE_PAGE - 1));
+}
